3e432b78b4b46009d3b9d95068f4afd51ce666e9
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-27 12:45:19 -0500

    Created borderChange function - still some bugs

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-var CarLot = (function(events){
+var CarLot = (function(populate){
 
 function populatePage (inventory) {
   // Loop over the inventory and populate the page
@@ -7,32 +7,50 @@ function populatePage (inventory) {
       var cars = inventory.cars;
       console.log("cars", cars)
       cars.forEach(function(car) {
-        console.log("car", car)
-        container.innerHTML +=createCar(car)
-      }); 
-    }
-
-function createCar(car) {
-   if (car.purchased === false) {
-      var purchased = `<span class="forSale"> For Sale! </span>`
-   } else {
-       var purchased = `<span class="sold"> SOLD </span>`
-   }
-   return `<div class="name col-sm-4">
-        <h2 class="carInfo"> ${car.make} ${car.model} <h2>
-        <p class="carYear"> ${car.year} </p>
-        <p class="carPrice"> Price: $${car.price} </p>
-        <p class="carsColor"> Color: ${car.color} </p>
-        <p class="description"> ${car.description} </p>
-        ${purchased}
-      </div>`
-  };
+        if (car.purchased === false) {
+         var purchased = `<span class="forSale"> For Sale! </span>`
+         } else {
+             var purchased = `<span class="sold"> SOLD </span>`
+         }
+        container.innerHTML += `<div class="col-sm-4" >
+          <div class="col-sm-12 carCard" style= 'border: 3px solid ${car.color}'>
+          <h2 class="carInfo"> ${car.make} ${car.model} <h2>
+          <p class="description"> ${car.description} </p>
+          <p class="carYear"> ${car.year} </p>
+          <p class="carsColor"> Color: ${car.color} </p>
+          <p class="carPrice"> Price: $${car.price} </p>
+           ${purchased}
+         </div>
+        </div>`
+      });
+    //   var border = document.querySelectorAll('.carCard');
+    //   border.forEach((card)=>{
+    //     var cars = inventory.cars
+    //     console.log("cars", cars)
+    //     console.log("border color", cars.color)
+    //     card.style.border = `1px solid ${cars.color}`
+    // })
+    //   borderColor(inventory);
+    activateEvents()
+  }
+  function activateEvents (inventory){
+    var cards = document.querySelectorAll(".carCard");
+    cards.forEach((card)=>{
+      card.addEventListener("click", (e)=>{
+        console.log(e)
+        var input = document.getElementById("inputText");
+        input.value = "";
+        console.log("clicked")
+        input.focus();
+        // var carCard = e.target.offsetParent;
+        console.log(e.target.offsetParent.style.cssText)
+        events(borderChange(e.target.offsetParent, car.color))
+      })
+    })
+  }
   // Now that the DOM is loaded, establish all the event listeners needed
-  // CarLot.activateEvents();
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-  console.log(populatePage)
+   carLot.activateEvents(populatePage);
    CarLot.loadInventory(populatePage);
-   return events;
+   return populate;
 }) (CarLot || {})
 
---
+++ styles.js
@@ -0,0 +1,11 @@
+var CarLot = (function createCar(styles) {
+
+function borderReset (){
+
+}
+
+function borderChange(card, color){
+  card.style.border = `5px solid ${color}`
+
+}
+}) (CarLot || {})
