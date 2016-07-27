02411d96d82aae708e43edab0bc8d63e6d42c22c
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-27 09:37:47 -0500

    Fixed IIFE and callback function so that the car information shows up on the page

--- carLot.js
+++ carLot.js
@@ -1,5 +1,5 @@
 var CarLot = (function () {
-  var inventory = [];
+  var inventory;
 
   return {
     getInventory: function () {
@@ -9,8 +9,7 @@ var CarLot = (function () {
       var inventoryLoader = new XMLHttpRequest();
       inventoryLoader.open('GET', 'inventory.json')
       inventoryLoader.addEventListener("load", function () {
-         inventory.push(JSON.parse(inventoryLoader.responseText))
-         console.log(data)
+         inventory = JSON.parse(inventoryLoader.responseText)
          callback(inventory);
       });
       inventoryLoader.send();
--- quiz.js
+++ quiz.js
@@ -1,31 +1,34 @@
 var CarLot = (function(events){
 
-function populatePage(inventory) {
+function populatePage (inventory) {
   // Loop over the inventory and populate the page
-    var cars = getInventory();
-    for (var i = 0; i<cars.length; i++){
+    console.log("inventory", inventory)
       var container = document.getElementById("carLot");
-      container.innerHTML += createCar(car);
+      var cars = inventory.cars;
+      console.log("cars", cars)
+      cars.forEach(function(car) {
+        console.log("car", car)
+         container.innerHTML += createCar(car)
+      });
     }
-  };
 
 function createCar(car) {
-  return `<div class="row cars">
-      <div class="name col-sm-4">
-        <h2 class="carInfo"> ${car.make} ${cars.model} <h2>
+  return `<div class="name col-sm-4">
+        <h2 class="carInfo"> ${car.make} ${car.model} <h2>
         <p class="carYear"> ${car.year} </p>
-        <p class="carPrice"> Price: ${car.price} </p>
+        <p class="carPrice"> Price: $${car.price} </p>
         <p class="carsColor"> Color: ${car.color} </p>
         <p class="carPurchased"> Purchased? ${car.purchased} </p>
         <p class="description"> ${car.description}
       </div>`
-};
+  };
   // Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
-}
+  // CarLot.activateEvents();
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
-   CarLot.loadInventory();
+  console.log(populatePage)
+   CarLot.loadInventory(populatePage);
+   return events;
 }) (CarLot || {})
 
 
