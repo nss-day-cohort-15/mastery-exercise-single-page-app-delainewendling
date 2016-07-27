3659b6fa33cb0431b37487731f818b5ef66e4439
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-27 11:06:01 -0500

    Styled the availability icons

--- quiz.js
+++ quiz.js
@@ -2,24 +2,29 @@ var CarLot = (function(events){
 
 function populatePage (inventory) {
   // Loop over the inventory and populate the page
+  var container = document.getElementById("carLot");
     console.log("inventory", inventory)
-      var container = document.getElementById("carLot");
       var cars = inventory.cars;
       console.log("cars", cars)
       cars.forEach(function(car) {
         console.log("car", car)
-         container.innerHTML += createCar(car)
-      });
+        container.innerHTML +=createCar(car)
+      }); 
     }
 
 function createCar(car) {
-  return `<div class="name col-sm-4">
+   if (car.purchased === false) {
+      var purchased = `<span class="forSale"> For Sale! </span>`
+   } else {
+       var purchased = `<span class="sold"> SOLD </span>`
+   }
+   return `<div class="name col-sm-4">
         <h2 class="carInfo"> ${car.make} ${car.model} <h2>
         <p class="carYear"> ${car.year} </p>
         <p class="carPrice"> Price: $${car.price} </p>
         <p class="carsColor"> Color: ${car.color} </p>
-        <p class="carPurchased"> Purchased? ${car.purchased} </p>
-        <p class="description"> ${car.description}
+        <p class="description"> ${car.description} </p>
+        ${purchased}
       </div>`
   };
   // Now that the DOM is loaded, establish all the event listeners needed
@@ -31,22 +36,3 @@ function createCar(car) {
    return events;
 }) (CarLot || {})
 
-
-
-// function getJSON (url, callback) {
-//   var xhr = new XMLHttpRequest()
-//   xhr.open('GET', url)
-//   xhr.addEventListener('load', function() {
-//     var data = JSON.parse(xhr.responseText)
-//     callback(data)
-//   })
-//   xhr.send()
-// }
-
-// getJSON('inventory.json', function(data){
-//   var cContainer = document.getElementById("carLot");
-//   var cars = data.cars;
-//   cars.forEach(function(car) {
-//     cContainer.innerHTML += createCar(car)
-//   });
-// });
